32baa2c5746ee64c832248780ae89ad931ecff48
joaolucas <joaolucasdeassis.o@gmail.com>
2024-07-21 14:19:04 -0300

    fix: Renomeado diretório raiz do projeto

---
+++ Middleware/Templates/TemplateMiddleware.cs
@@ -0,0 +1,21 @@
+﻿namespace TemplateMiddleware.Templates;
+
+public class TemplateMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public TemplateMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        // Do something first
+
+        // Call the next middleware in the pipeline
+        await _next(context);
+
+        // Do something later
+    }
+}
