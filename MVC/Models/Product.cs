5ce1a0886c8adf6772d348eac5eb5cd549fe4ae1
joaolucas <joaolucasdeassis.o@gmail.com>
2024-07-21 14:19:28 -0300

    feat: Adicionado template de um projeto MVC

---
+++ MVC/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace MVC.Models;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public bool IsAvailable { get; set; }
+}
