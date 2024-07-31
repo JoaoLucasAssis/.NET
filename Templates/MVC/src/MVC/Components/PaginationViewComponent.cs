347be3ed6c55d9b3eed907c25309385119b4cf94
joaolucas <joaolucasdeassis.o@gmail.com>
2024-07-30 23:23:50 -0300

    feat: Adicionado paginação no projeto MVC

---
+++ Templates/MVC/src/MVC/Components/PaginationViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC.Components;
+
+public class PaginationViewComponent : ViewComponent
+{
+    public IViewComponentResult InvokeAsync(int totalItems, int currentPage, int itemsPerPage)
+    {
+        int totalPages = (int)Math.Ceiling((double)totalItems / itemsPerPage);
+
+        return View(new
+        {
+            CurrentPage = currentPage,
+            TotalPages = totalPages
+        });
+    }
+}
