ea69a01ee9551eae9ffe489049352264b1448352
joaolucas <joaolucasdeassis.o@gmail.com>
2024-08-01 18:38:05 -0300

    feat: Adicionado caixa de pesquisa para clientes e produtos

---
+++ Templates/MVC/src/MVC/Components/SearchBoxViewComponent.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC.Components;
+
+public class SearchBoxViewComponent : ViewComponent
+{
+    public async Task<IViewComponentResult> InvokeAsync(string controller, string searchTerm = "")
+    {
+        return View(new
+        {
+            Controller = controller,
+            SearchTerm = searchTerm
+        });
+    }
+}
