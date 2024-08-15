3d73617de8a7f4b883c5efd50a141c905a6e3a1b
joaolucas <joaolucasdeassis.o@gmail.com>
2024-08-15 19:52:08 -0300

    fix: Corrigido upload de imagens de produtos Removido método privado na controller de produtos para upload. Criado um serviço com o método e utilizado como injeção de dependência

---
+++ Templates/MVC/src/MVC/Services/IImageUploadService.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace MVC.Services;
+
+public interface IImageUploadService
+{
+    public Task<bool> UploadProductImage(ModelStateDictionary modelState, IFormFile file, string imgPrefix);
+}
---
+++ Templates/MVC/src/MVC/Services/ImageUploadService.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace MVC.Services
+{
+    public class ImageUploadService : IImageUploadService
+    {
+        public async Task<bool> UploadProductImage(ModelStateDictionary modelState, IFormFile file, string imgPrefix)
+        {
+            if (file.Length <= 0) return false;
+
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images", imgPrefix + file.FileName);
+
+            try
+            {
+                var directory = Path.GetDirectoryName(path);
+                if (!Directory.Exists(directory))
+                {
+                    Directory.CreateDirectory(directory);
+                }
+
+                using (var stream = new FileStream(path, FileMode.Create))
+                {
+                    await file.CopyToAsync(stream);
+                }
+            }
+            catch (Exception ex)
+            {
+                modelState.AddModelError(string.Empty, $"An error occurred while saving the file: {ex.Message}");
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
